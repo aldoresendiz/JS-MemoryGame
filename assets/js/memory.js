a1014c6c1208e564b08574e3a0b5101fb8dee402
Aldo Resendiz <aldoresendiz@gmail.com>
2023-09-27 11:30:17 -0500

    Assigned sounds to each button plus refactored the code to start the sound right as the button is cliked

--- assets/js/memory.js
+++ assets/js/memory.js
@@ -1,11 +1,61 @@
-function configureListeners() {
-    let images = document.querySelectorAll('img'); // select img elements  
+// define levels
+let currentLevel = 1;
+let maxLevel = 1;
+//define array with queue of buttons to follow
+let currentGame = []
+// currentPlayer 0:Computer, 1:User
+let currentPlayer = 0;
+// configuration sounds
+let tempo = 100; //100 beats per minute is the default
 
-    images.forEach((i) => {
-            document.getElementById(i.id).addEventListener('mouseover', addOpacity, false)
-            document.getElementById(i.id).addEventListener('mouseout', removeOpacity, false)
-        })
+const doReMiFaSo = [
+    {note: "C4", duration: 0.5, frequency: 261.63, boton: 'pn1'},
+    {note: "D4", duration: 0.5, frequency: 293.66, boton: 'pn2'},
+    {note: "E4", duration: 0.5, frequency: 329.63, boton: 'pn3'},
+    {note: "F4", duration: 0.5, frequency: 349.23, boton: 'pn4'},
+    {note: "G4", duration: 0.5, frequency: 392.00, boton: 'pn5'},
+    {note: "A4", duration: 0.5, frequency: 440.00, boton: 'pn6'},
+    {note: "B4", duration: 0.5, frequency: 493.88, boton: 'pn7'},
+    {note: "C5", duration: 0.5, frequency: 523.25, boton: 'pn8'},
+    {note: "C4", duration: 0.5, frequency: 261.63, boton: 'pn9'},
+  ];
+
+function resetGame() {
+    currentLevel = 1;
+    maxLevel = 1;
+    currentGame = [];
+    currentPlayer = 0;
+    tempo = 100;
 }
+
+function configureListeners() {
+    let images = document.querySelectorAll('img');
+    images.forEach((i) => {
+        document.getElementById(i.id).addEventListener('click', playSound, false);
+    })
+    let boton = document.querySelector('#btn-start');
+    boton.addEventListener('click', resetGame, false);
+}
+
+function playSound(event) {
+    buttonClicked = event.target.id;
+    doReMiFaSo.forEach((note) => {
+    if (note.boton == buttonClicked)
+    {
+        const context = new (window.AudioContext || window.webkitAudioContext)();
+        let waveforms = ["sine", "square", "sawtooth", "triangle"];
+        var quarterNoteTime = 60 / tempo; //60 is the default
+        var endTime = note.duration * quarterNoteTime;
+        var oscillator = context.createOscillator();
+        oscillator.type = "sine";
+        var noteFrequency = note.frequency;
+        oscillator.frequency.value = noteFrequency;
+        oscillator.connect(context.destination);
+        oscillator.start(context.currentTime);
+        oscillator.stop(context.currentTime + endTime);
+    }
+    });
+};
 
 function addOpacity(event) {
     // add appropriate CSS class
